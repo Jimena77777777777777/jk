f13bbb83985bab59250ff016080f1e248760bba0
Alee <202100355D@utea.edu.pe>
2024-01-11 09:05:41 -0500

    Primer commit

---
+++ ejem.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejemplo2</title>
+</head>
+<body>
+    
+    <header>
+    <h3>FUNDAMENTOS DE PROGRAMACION</h6> 
+
+    </header>
+
+    <section>
+
+
+    </section>
+
+    <footer>
+        <h6>las mil y una noches</h6>
+    </footer>
+</body>
+</html>
