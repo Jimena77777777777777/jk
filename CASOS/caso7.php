f0069bf68380eb4ef47358927aeadb8a3e855073
Alee <202100355D@utea.edu.pe>
2024-01-25 00:41:21 -0500

    EEE

---
+++ CASOS/caso7.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University</title>
+    <link rel="stylesheet" href="estilocaso.css">
+
+    <style>
+        body{
+            font-family:sans-serif;
+        }
+        .contenedor{
+            background-color: palevioletred ;
+            height: 50vh;
+            margin: 6%;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <header>
+    <div class= "Contenedor">
+        <h1> MENSUALIDAD UNIVERSIDAD </h1>
+    </div>
+    
+
+    <div class="alinear_Flex">
+    <img src="../imagenes/university.jpg" width="564" height="391">    
+    </div>
+    </header>
+    <section></section>
+</body>
+</html>
